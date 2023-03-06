2f5d3938987105fe83336767c8f2885f6b01a394
Suraj <surajbasnet449@gmail.com>
2023-03-06 16:24:33 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [transcript, setTranscript] = useState("");
+
+  const handleListen = () => {
+    const recognition = new window.SpeechRecognition();
+    recognition.start();
+    recognition.onresult = (event) => {
+      setTranscript(event.results[0][0].transcript);
+    };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Speech Recognition App</h3>
+      <button onClick={handleListen}>Start Listening</button>
+      <p>{transcript}</p>
     </div>
   );
 }
